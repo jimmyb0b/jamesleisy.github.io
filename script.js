c53be681f29f2b33c55e5b74c018360f6065e710
James Leisy <jamesleisy@gmail.com>
2015-07-16 13:06:52 -0400

    cards shuffled

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log("linked")
+
+
+var cardSuits = ['C','D','H','S']
+var cardNumbers = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
+//var cardLocation = []
+var cardImg = []
+
+// var assigningCountValue = function(){	
+// 	for (i=0; i<52; i++){
+// 		cardLocation.push(i)
+// 	}
+//	console.log(cardLocation)
+//}
+// assigningLocationValue()
+
+var Card = function Card(cardSuits,cardNumbers){
+this.cardSuits = cardSuits;
+this.cardNumbers = cardNumbers;
+//this.cardLocation = cardLocation
+}
+
+var deck = []
+	var createCards = function(){
+		for (i=0; i < cardSuits.length; i++) {
+			for (o=0;o<cardNumbers.length;o++){		
+				deck.push(new Card(cardSuits[i],cardNumbers[o]))
+			}
+		}
+	}
+createCards()
+
+var shuffleDeck = []
+	var randomizedDeck = function(){
+		for (i=0; i<deck.length; i++){
+				shuffling = Math.floor(Math.random()* this.deck.length)
+				shuffleDeck.push(deck[shuffling])
+			}		
+		console.log(shuffleDeck)
+	}
+randomizedDeck()
+
+
+
+
+//.shift
+//.pop
