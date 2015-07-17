7ebd7936747b8b5b544b4f60c2a36201138d7540
James Leisy <jamesleisy@gmail.com>
2015-07-17 15:04:52 -0400

    committing

--- script.js
+++ script.js
@@ -3,40 +3,40 @@ console.log("linked")
 
 var cardSuits = ['C','D','H','S']
 var cardNumbers = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
-var cardValue = []
 var cardImg = ["2_of_clubs","2_of_diamonds","2_of_hearts","2_of_spades","3_of_clubs","3_of_diamonds","3_of_hearts","3_of_spades","4_of_clubs","4_of_diamonds","4_of_hearts","4_of_spades","5_of_clubs","5_of_diamonds","5_of_hearts","5_of_spades","6_of_clubs","6_of_diamonds","6_of_hearts","6_of_spades","7_of_clubs","7_of_diamonds","7_of_hearts","7_of_spades","8_of_clubs","8_of_diamonds","8_of_hearts","8_of_spades","9_of_clubs","9_of_diamonds","9_of_hearts","9_of_spades","10_of_clubs","10_of_diamonds","10_of_hearts","10_of_spades","jack_of_clubs2","jack_of_diamonds2","jack_of_hearts2","jack_of_spades2","queen_of_clubs2","queen_of_diamonds2","queen_of_hearts2","queen_of_spades2","king_of_clubs2","king_of_diamonds2","king_of_hearts2","king_of_spades2","ace_of_clubs","ace_of_diamonds","ace_of_hearts","ace_of_spades2"]
+var cardValue = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,11,11,11,11,]
 
 
-var Card = function Card(cardSuits,cardNumbers,cardImg){
+var Card = function Card(cardSuits,cardNumbers,cardImg,cardValue){
 this.cardSuits = cardSuits;
 this.cardNumbers = cardNumbers;
-this.cardValue = cardValue;
 this.cardImg = cardImg;
+this.cardValue = cardValue;
 }
 
 
-var deckImg = []
-var assignCardImages = function(){
-	for (i=0;i<cardImg.length; i++)
-		deckImg.push(new Card("playing_cards/"+cardImg[i]))
+// var deckImg = []
+// var assignCardImages = function(){
+// 	for (i=0;i<cardImg.length; i++)
+// 		deckImg.push(new Card())
 //"playing_cards/"+
-}
+//}
 
 //deck.push(cardImg(deckImg))
-assignCardImages()
+//assignCardImages()
 
 var deck = []
 	var createCards = function(){
 
-		for (i=0; i < cardSuits.length; i++) {
-			for (o=0;o<cardNumbers.length;o++){	
-				deck.push(new Card(cardSuits[i],cardNumbers[o]))
+			for (i=0; i < cardSuits.length; i++) {
+				for (o=0; o< cardNumbers.length;o++){	
+	//				for (u=0; u<this.cardImg.length;u++){
+					deck.push(new Card(cardSuits[i],cardNumbers[o],('playing_cards/'+cardImg[i+4*o]),(cardValue[i+4*o])))
+				}
 			}
+			console.log(deck)
 		}
-		// for (u=0;u<deckImg.length;u++){
-		// 	deck.push(new Card(deckImg[u]))	
-		//}
-	}
+
 createCards()
 
 
@@ -69,19 +69,52 @@ dealCards()
 
 
 var Game = function(){
-	this.render = function(element,playerHand,dealerHand){
+	this.render = function(){
 		$('.card-deck').append('<img class="card-stack" src="https://s3.amazonaws.com/ksr/assets/000/246/302/979a46ed6000168e916a6ef1230c9afb_large.jpg">')
-		$p = $('<p class="player-hand">').text(element.playerHand())
-		$d = $('<p class="dealer-hand">').text(element.dealerHand())
-			$('.dealer-hand').append($d)//('<img class="dealerCards" src=""')
+		for (i=0; i<playerHand.length;i++){
+			$p = $('<p class="player-cards">').push(cardImg[i])
 			$('.player-hand').append($p)//('<img class="playCards" src=""')
+		}
+		for (i=0; i<dealerHand.length;i++){
+			$d = $('<p class="dealer-cards">').push(cardImg[i])
+			$('.dealer-hand').append($d)//('<img class="dealerCards" src=""')
+		}
+			console.log(playerHand)
+			console.log(dealerHand)
 
 	}
-
 }
 
-var game = new Game(shuffleDeck,dealCards,dealerHand,playerHand)
+
+var game = new Game(shuffleDeck,playerHand,dealerHand)
 game.render()
+
+
+
+var playerScore = function(playerHand){
+	var score = 0
+	for (i=0; i< playerHand.length;i++){
+		score+=(parseInt(this.Card[i].cardNumbers))
+		if (playerHand.contains("A") && score >21){
+			Card[i].cardValue = 1
+	}
+}
+	console.log(playerScore)
+}
+playerScore(playerHand)
+
+
+var dealerScore = function(dealerHand){
+	var score = 0
+	for (i=0; i< dealerHand.length;i++){
+		score+=(parseInt(this.Card[i].cardNumbers))
+		if (dealerHand.contains("A") && score >21){
+			Card[i].cardValue = 1
+	}
+}
+	console.log(dealerScore)
+}
+dealerScore(dealerHand)
 
 
 
